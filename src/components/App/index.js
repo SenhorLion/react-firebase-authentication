a73fa43c7058646475e43d7a19e997dc131b446a
Lionel Adams <adamslionel@hotmail.co.uk>
2019-02-01 20:04:54 +0000

    React-router, Routes and Navigation setup

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navigation from '../Navigation';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
+import LandingPage from '../Landing';
+import HomePage from '../Home';
+
+import * as ROUTES from '../../constants/routes';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <div>
+        <Navigation />
+
+        <hr />
+
+        <Route exact path={ROUTES.LANDING} component={LandingPage} />
+        <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
+        <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+        <Route
+          exact
+          path={ROUTES.PASSWORD_FORGET}
+          component={PasswordForgetPage}
+        />
+        <Route exact path={ROUTES.HOME} component={HomePage} />
+        <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
+        <Route exact path={ROUTES.ADMIN} component={AdminPage} />
+      </div>
+    </Router>
+  );
 };
 
 export default App;
